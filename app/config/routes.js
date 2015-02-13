e73c77563e7a3fca632685aa361622cb0bf8069d
Franky Chung <franchun@gmail.com>
2015-02-13 22:15:52 +0900

    Persist Inside and Outside

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,12 +1,19 @@
 var React = require('react/addons');
-var { Route, DefaultRoute } = require('react-router');
+var { Route } = require('react-router');
 
 var AppHandler = require('../screens/App/index');
 var FrontHandler = require('../screens/App/screens/Front/index');
 
+/**
+ * Normally you would use a default route there where FrontHandler is but
+ * couldn't get the top level handler to listen to changes on the store. But I
+ * don't want the Inside and Outside components to change as paths change so
+ * this seems to work.
+ */
 var routes = (
   <Route handler={AppHandler} path="/">
-    <DefaultRoute name="front" handler={FrontHandler} />
+    <Route path="/" name="front" handler={FrontHandler}>
+    </Route>
   </Route>
 );
 
--- app/screens/App/screens/Front/index.js
+++ app/screens/App/screens/Front/index.js
@@ -2,6 +2,7 @@
 
 var React = require('react/addons');
 var { PureRenderMixin } = React.addons;
+var { RouteHandler } = require('react-router');
 
 var StoresMixin = require('mixins/StoresMixin');
 var userStore = require('stores/userStore');
@@ -57,11 +58,13 @@ var FrontHandler = React.createClass({
   render: function() {
     var content = this.state.user.get('auth') ?
       <Inside user={this.state.user}>
+        <RouteHandler />
         <a href="#" onClick={this.handleClickLogOut}>
           Logout
         </a>
       </Inside> :
       <Outside user={this.state.user}>
+        <RouteHandler />
         <Register />
       </Outside>;
 
